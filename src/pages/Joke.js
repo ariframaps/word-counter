9e72d666fc7f53eacc54bad2a2a098ad1378d7f8
ariframaps <ariframa@student.ub.ac.id>
2024-04-27 22:32:58 +0700

    chuck norris joke generate

--- src/pages/Joke.js
+++ src/pages/Joke.js
@@ -1,12 +1,24 @@
+import { useEffect, useState } from "react"
+
 export const Joke = () => {
+    const [joke, setJoke] = useState('')
+
+    useEffect(() => {
+        async function generateJoke() {
+            const response = await fetch('https://api.chucknorris.io/jokes/random')
+            const data = await response.json()
+            setJoke(data.value)
+        }
+        generateJoke()
+    }, [])
+
     return (
         <main className="h-screen p-24 text-center">
             <div className="max-w-5xl mx-auto">
-                <h1 className="text-3xl font-bold my-10">Word Counter</h1>
+                <h1 className="text-3xl font-bold my-10">Chuck norris joke</h1>
                 <div className="border border-black p-5">
-                    this is the joke - <a href="https://api.chucknorris.io/" target="_blank" rel="noreferrer">api.chucknorris.io</a>
+                    {joke} - <a href="https://api.chucknorris.io/" target="_blank" rel="noreferrer">api.chucknorris.io</a>
                 </div>
-                <button className="my-16 p-3 rounded-md bg-blue-600 hover:bg-blue-700 text-white font-bold px-5">Generate joke</button>
             </div>
         </main>
     )
