ccaf1fe9ed097880e4358dfcbef0de527d8941ca
ariframaps <ariframa@student.ub.ac.id>
2024-04-27 22:25:46 +0700

    joke interface

--- src/pages/Joke.js
+++ src/pages/Joke.js
@@ -1,5 +1,13 @@
 export const Joke = () => {
     return (
-        <div>Joke</div>
+        <main className="h-screen p-24 text-center">
+            <div className="max-w-5xl mx-auto">
+                <h1 className="text-3xl font-bold my-10">Word Counter</h1>
+                <div className="border border-black p-5">
+                    this is the joke - <a href="https://api.chucknorris.io/" target="_blank" rel="noreferrer">api.chucknorris.io</a>
+                </div>
+                <button className="my-16 p-3 rounded-md bg-blue-600 hover:bg-blue-700 text-white font-bold px-5">Generate joke</button>
+            </div>
+        </main>
     )
 }
