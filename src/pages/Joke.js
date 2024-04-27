b5ffb2fe4867b8a7bf69e2f85959b76635b4a7d0
ariframaps <ariframa@student.ub.ac.id>
2024-04-27 21:35:56 +0700

    project files initialization

---
+++ src/pages/Joke.js
@@ -0,0 +1,5 @@
+export const Joke = () => {
+    return (
+        <div>Joke</div>
+    )
+}
---
+++ src/pages/WordCounter.js
@@ -0,0 +1,5 @@
+export const WordCounter = () => {
+    return (
+        <div>WordCounter</div>
+    )
+}
